e36185a54e58d376cf5e9f1ea69fa221206dbae6
Prakki Rama <ramadatta.88@gmail.com>
2019-12-27 13:01:09 +0800

    Add files via upload

---
+++ app.R
@@ -0,0 +1,117 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+#install.packages("psych")
+library(shiny)
+library(data.table)
+library(dplyr)
+library(psych)
+if (interactive()) {
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Find Arithmatic, Geometric, Harmonic Mean and Median at one shot"),
+
+      # Sidebar layout with input and output definitions ----
+   sidebarLayout(
+     
+     # Sidebar panel for inputs ----
+     sidebarPanel(
+       
+       # Horizontal line ----
+       tags$hr(),
+       # Input: Select a file ----
+       fileInput("file1", "Choose CSV File",
+                 multiple = FALSE,
+                 accept = c("text/csv",
+                            "text/comma-separated-values,text/plain",
+                            ".csv")),
+       
+       # Horizontal line ----
+       tags$hr(),
+       
+       # Input: Select number of rows to display ----
+       radioButtons("disp", "Display",
+                    choices = c(Head = "head",
+                                All = "all"),
+                    selected = "head", inline = TRUE),
+       
+       # Horizontal line ----
+       tags$hr(),
+       
+       # Button
+       downloadButton("downloadData", "Download")
+       
+     ),
+     
+     # Main panel for displaying outputs ----
+     mainPanel(
+       
+       # Output: Data file ----
+    #   plotOutput("distPlot"),
+       
+     #  plotOutput("distPlot2"),
+       
+       tableOutput("contents")
+       
+     )
+   ),
+
+   fluidRow(column(8, align="left",h3("example dataset", a("here", href="https://google.com")))) 
+
+)
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$contents <- renderTable({
+    
+    req(input$file1)
+    
+    dd <- fread(input$file1$datapath)
+
+      summarydf <- 
+      dd %>% 
+      group_by(V1) %>% 
+      summarise(Arith.MashMean=mean(V2), Geo.MashMean=exp(mean(log(V2))), Harmonic.MashMean=harmonic.mean(V2), MashMedian=median(V2))
+      
+       if(input$disp == "head") {
+        return(head(summarydf))
+      }
+      else {
+        return(summarydf)
+      }
+      })
+  
+  sample_data <- reactive({
+    
+    req(input$file1)
+    
+    dd <- fread(input$file1$datapath)
+    
+    summarydf <- 
+      dd %>% 
+      group_by(V1) %>% 
+      summarise(Arith.MashMean=mean(V2), Geo.MashMean=exp(mean(log(V2))), Harmonic.MashMean=harmonic.mean(V2), MashMedian=median(V2))
+    })
+  # Downloadable csv of selected dataset ----
+ 
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste('data-', Sys.Date(), '.txt', sep='')
+    },
+    content = function(file) {
+      write.table(sample_data(), file, sep = "\t", row.names = FALSE)
+    }
+  )
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+}
