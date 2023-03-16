5a35d3142ae5c751340e250c9498622066fe021b
Prakki Rama <ramadatta.88@gmail.com>
2023-03-16 11:26:49 +0800

    Create readme.R

---
+++ plasmidMaps/readme.R
@@ -0,0 +1,4 @@
+take a genbank and generate BRIG like figure
+Color code the genes into AMR, IS, Transposons, Insertion Sequences, Virulence genes, Hypothetical Proteins
+If any genes need to colored those genes should be passed
+Shiny app - we can select AMR, IS, Transposons, Insertion Sequences, Virulence genes, Hypothetical Proteins the option we want to see on the circular genome
